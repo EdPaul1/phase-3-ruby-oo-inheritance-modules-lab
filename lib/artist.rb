15bbf50e011663b1b8f96160111a6560ceeba965
EdPaul1 <saidarajab101@gmail.com>
2023-05-26 13:04:52 +0300

    Modules,extend,include,super,name::spacing

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,54 +1,33 @@
-require 'pry'
-require relative '../lib/concerns/memorable'
-require relative '../lib/concerns/paramable'
-
-
 class Artist
-  attr_accessor :name
-  attr_reader :songs
+  attr_accessor :name, :songs
 
-  extend reset_all
-  extend count
-
-  include paramable
+  extend Findable
+  include Paramable
 
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
 
-  @@artists = []
+  @@all = []
 
   def initialize
     super
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def self.all
-    @@artists
+    @@all
   end
 
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
+  # def self.find_by_name(name)
+  #   @@artists.detect {|a| a.name == name}
   # end
 
   def add_song(song)
     @songs << song
-    song.artist = self
   end
 
   def add_songs(songs)
-    songs.each { |song| add_song(song) }
+    @songs.concat(songs)
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,14 @@
-require 'pry'
-require relative '../lib/concerns/memorable'
-require relative '../lib/concerns/paramable'
-
 
 class Song
   attr_accessor :name
   attr_reader :artist
   
+  extend Findable
+  include Paramable
 
-  extend reset_all
-  extend count
-  include paramable
+  # extend reset_all
+  # extend count
+  # include paramable
 
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
@@ -22,27 +20,16 @@ class Song
   #   @@songs << self
   # end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def artist=(artist)
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
